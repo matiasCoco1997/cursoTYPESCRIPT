3781eb9f87390f49db3d7aaba2cfdbeb6a53f1a0
Matias Ariel Coco <cocomatias6@gmail.com>
2023-12-27 11:02:48 -0300

    update fin de unidad datos primitivos

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-"use strict";
-console.log("Hola mundo con Typescript");
+var datoBooleano = true;
+if (datoBooleano) {
+    console.log("el valor es verdadero");
+}
+else {
+    console.log("el valor es falso");
+}
+var interMiami = 11;
+var fcDallas = 11;
+var messi = 1;
+var juegaMessi = true;
+var palabras = "Me emocione al verlo a Messi";
+function jugar(equipo1, equipo2, juegaMessi) {
+    var motivo = "";
+    if (juegaMessi) {
+        equipo1 += messi;
+        motivo = " porque juega Messi";
+    }
+    if (equipo1 > equipo2)
+        console.log("Gana Inter Miami ".concat(motivo));
+    if (equipo1 < equipo2)
+        console.log("Gana FC Dallas");
+    if (equipo1 == equipo2)
+        console.log("Empataron");
+}
+jugar(interMiami, fcDallas, juegaMessi);
--- script.ts
+++ script.ts
@@ -1,1 +1,33 @@
-console.log("Hola mundo con Typescript")
+let datoBooleano: boolean = true
+
+if(datoBooleano){
+    console.log("el valor es verdadero")
+} else{
+    console.log("el valor es falso")
+}
+
+let interMiami:number | null | undefined = 11
+let fcDallas:number = 11
+let messi: number = 1
+let juegaMessi: boolean = true
+
+let palabras:string = "Me emocione al verlo a Messi"
+
+function jugar (equipo1:number, equipo2:number, juegaMessi:boolean ) : void {
+    
+    let motivo: string = ""
+
+    if(juegaMessi){
+        equipo1 += messi
+        motivo = " porque juega Messi"
+    } 
+
+    if( equipo1 > equipo2 ) console.log(`Gana Inter Miami ${motivo}`)
+
+    if( equipo1 < equipo2 ) console.log("Gana FC Dallas")
+
+    if( equipo1 == equipo2 ) console.log("Empataron")
+
+}
+
+jugar(interMiami, fcDallas, juegaMessi);
